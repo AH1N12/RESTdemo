edec628ed0f3d716c10a452d37c62b2cac3a03d1
AH1N1 <wolf11-1995@o2.pl>
2017-10-17 23:49:28 +0200

    some co mmit

---
+++ src/main/java/Rest/demo/RESTdemo/ResTdemoApplication.java
@@ -0,0 +1,12 @@
+package Rest.demo.RESTdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTdemoApplication.class, args);
+	}
+}
